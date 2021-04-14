c93160c9cd35aa2d3fb1ba864071717e19bdade5
nami <je191030@ens.uqam.ca>
2021-04-14 01:18:40 -0400

    Version 4.50 : Pipes fixed + delim done.

--- pat.c
+++ pat.c
@@ -19,12 +19,13 @@ typedef struct pat_s {
     int posDelD;
     int posDelF;
     char **newCmd;
-    struct pollfd fds[3];
-    int std[2];
-    int stdO[2];
-    int stdE[2];
     int sig;
     int nbrArgs;
+    struct pollfd fds[4];
+    int stdExit[2];
+    int stdOut[2];
+    int stdErr[2];
+    int stdNum[2];
 } pat_t;
 
 void cmds(char **, const int *, pat_t *);
@@ -32,7 +33,7 @@ void execCmds(pat_t *pat);
 void pPoll(pat_t *pat);
 int forking(pat_t *pat, int, char **argv);
 void polling(pat_t *);
-void printStat(int retour, pat_t *pat);
+void printExit(int retour, pat_t *pat);
 void exitStd(pat_t *);
 
 int main(int argc, char *argv[]) {
@@ -42,7 +43,8 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     pat_t *pat = malloc(sizeof(struct pat_s));
-    *pat = (struct pat_s) {0, "+", 0, 0, 0, {0}, {0}, {0}, {0}, 0,0};
+    *pat = (struct pat_s) {0, "+", 0, 0, 0, 0, 0};
+    *pat->fds = (struct pollfd) {0};
     if (strcmp(argv[1], "-s") == 0 && strlen(argv[2]) >= 1) {
         pat->delim = argv[2];
         pat->option = 2;
@@ -81,23 +83,34 @@ int forking(pat_t *pat, const int argc, char **argv) {
     if (pid == -1) { perror("Erreur fatale survenu!"); retour = 1; }
     else if (pid > 0) {
         polling(pat);
-        exitStd(pat);
-        if(waitpid(0,&pid,0) != -1) retour = WEXITSTATUS(pid);
+        if(waitpid(0,&pid,0) != -1) {
+            exitStd(pat);
+            retour = WEXITSTATUS(pid);
+        }
     } else if(pid == 0) {
+        int parent = getpid();
         for (int i = 1 + pat->option; i < argc; ++i) {
             pat->posDelF = 0;
             pat->posDelD = i;
             cmds(argv, &argc, pat);
             i += pat->posDelF;
-            pid = fork();
-            if(pid == 0) execCmds(pat);
+            pid_t neveu = fork();
+            if(neveu == 0) execCmds(pat);
+            if (pat->nbrArgs > 0) {
+                char *buf = calloc(1, sizeof(int));
+                sprintf(buf, "%d", neveu - parent);
+                if (write(pat->stdNum[1], buf, sizeof(buf)) == -1) {
+                    free(buf);
+                    _exit(1);
+                }
+            }
         }
-        while(waitpid(0,&pid,0) != -1) {
+        while(waitpid(-1,&pid,0) != -1) {
             if (WIFSIGNALED(pid)) {
-                pat->sig = 128 + WTERMSIG(pid);
-                retour += pat->sig;
+                pat->sig = WTERMSIG(pid);
+                retour += 128 + pat->sig;
             }  else if(WIFEXITED(pid)) retour += WEXITSTATUS(pid);
-            printStat(WEXITSTATUS(pid), pat);
+            printExit(WEXITSTATUS(pid), pat);
             pat->sig = 0;
         }
         exitStd(pat);
@@ -109,9 +122,8 @@ int forking(pat_t *pat, const int argc, char **argv) {
 void execCmds(pat_t *pat) {
 
     if (pat->newCmd[0]) {
-        dup2(pat->stdE[1], STDERR_FILENO);
-        dup2(pat->stdO[1], STDOUT_FILENO);
-        exitStd(pat);
+        dup2(pat->stdErr[1], STDERR_FILENO);
+        dup2(pat->stdOut[1], STDOUT_FILENO);
         execvp(pat->newCmd[0], pat->newCmd);
         perror(pat->newCmd[0]);
         free(pat->newCmd);
@@ -120,74 +132,88 @@ void execCmds(pat_t *pat) {
     }
 }
 
-void printStat(int retour, pat_t *pat) {
+void printExit(int retour, pat_t *pat) {
 
-    char *exit = malloc(strlen(pat->delim) * 3 + 20);
-    if (pat->sig == 0) sprintf(exit, "%s%s%s exit, status=%d\n", pat->delim, pat->delim, pat->delim, retour);
+    char *exit = malloc(strlen(pat->delim) + 12);
+    if (pat->sig == 0) sprintf(exit, "status=%d\n", retour);
     else {
-        sprintf(exit,"%s%s%s exit, signal=%d\n", pat->delim, pat->delim, pat->delim, pat->sig);
+        sprintf(exit,"signal=%d\n", pat->sig);
         free(pat->newCmd);
     }
-    write(pat->std[1], exit, strlen(exit));
+    write(pat->stdExit[1], exit, strlen(exit));
     free(exit);
 }
 
+
 void polling(pat_t *pat) {
 
-    close(pat->stdO[1]); close(pat->stdE[1]); close(pat->std[1]);
-    while(poll(pat->fds, 3, -1) > 0) {
+    close(pat->stdNum[1]);
+    size_t size = 0;
+    char *std;
+    struct pollfd fd;
+    char *sep = calloc(4, sizeof(pat->delim));
+    printf("%d\n", getpid());
+    snprintf(sep, sizeof(sep),"%s%s%s", pat->delim, pat->delim, pat->delim);
+    while(poll(pat->fds, 4, -1) > 0) {
+
         char buf[4096] = {0};
-        size_t size = 0;
-        if (pat->fds[0].revents & POLLIN) {
-            size = read(pat->stdO[0], buf, sizeof(buf));
-            if(size > 0) {
-                if (pat->nbrArgs > 0) {
-                    printf("%s%s%s stdout ", pat->delim, pat->delim, pat->delim);
-                    fflush(stdout);
-                } else printf("%s%s%s stdout\n", pat->delim, pat->delim, pat->delim);
-                write(1, buf, strlen(buf));
-                fflush(stdout);
-            }
+        int numCmd;
+        if(pat->fds[3].revents & POLLIN) {
+            size = read(pat->stdNum[0], buf, sizeof(buf));
+            if(size > 0) numCmd = (int) strtol(buf,NULL,0);
         }
-        if (pat->fds[1].revents & POLLIN) {
-            size = read(pat->stdE[0], buf, sizeof(buf));
-            if(size > 0) {
+        for(int i = 1; i < 4; ++i) {
+            if(i == 1) { std = "stdout"; fd = pat->fds[1]; }
+            else if(i == 2) { std = "stderr"; fd = pat->fds[2]; }
+            else { std = "exit"; fd = pat->fds[0]; }
+
+            if (fd.revents & POLLIN) {
                 if (pat->nbrArgs > 0) {
-                    printf("%s%s%s stderr ", pat->delim, pat->delim, pat->delim);
-                    fflush(stdout);
+                    if(fd.fd != pat->stdExit[0]) printf("%s %s %d\n", sep, std, numCmd);
+                    else printf("%s %s %d, ", sep, std, numCmd);
                 }
-                else printf("%s%s%s stderr\n", pat->delim, pat->delim, pat->delim);
-                write(1, buf, strlen(buf));
+                else printf("%s %s\n", sep, std);
+                size = read(fd.fd, buf, sizeof(buf));
+                if (size > 0 && buf[size - 1] != '\n')
+                    snprintf(sep, sizeof(sep), "%s%s%s%s", pat->delim, pat->delim, pat->delim, pat->delim);
+                else snprintf(sep, sizeof(sep), "%s%s%s", pat->delim, pat->delim, pat->delim);
+                if (size > 0 && buf[size - 1] != '\n') printf("%s\n", buf);
+                else if (size > 0) printf("%s", buf);
                 fflush(stdout);
             }
-        }
-        if (pat->fds[2].revents & POLLIN) {
-            size = read(pat->std[0], buf, sizeof(buf));
-            if(size > 0) write(1, buf, strlen(buf));
         }
         if(size == 0) break;
+        size = 0;
     }
+    free(sep);
 }
+
+
 
 void pPoll(pat_t *pat) {
 
-    pipe(pat->std);
-    pipe(pat->stdO);
-    pipe(pat->stdE);
-    pat->fds[0].fd = pat->stdO[0];
+    pipe(pat->stdExit);
+    pipe(pat->stdOut);
+    pipe(pat->stdErr);
+    pipe(pat->stdNum);
+    pat->fds[0].fd = pat->stdExit[0];
     pat->fds[0].events = POLLIN;
-    pat->fds[1].fd = pat->stdE[0];
+    pat->fds[1].fd = pat->stdOut[0];
     pat->fds[1].events = POLLIN;
-    pat->fds[2].fd = pat->std[0];
+    pat->fds[2].fd = pat->stdErr[0];
     pat->fds[2].events = POLLIN;
+    pat->fds[3].fd = pat->stdNum[0];
+    pat->fds[3].events = POLLIN;
 }
 
 void exitStd(pat_t *pat) {
 
-    close(pat->std[0]);
-    close(pat->std[1]);
-    close(pat->stdO[0]);
-    close(pat->stdE[0]);
-    close(pat->stdO[1]);
-    close(pat->stdE[1]);
+    close(pat->stdExit[1]);
+    close(pat->stdExit[0]);
+    close(pat->stdOut[1]);
+    close(pat->stdOut[0]);
+    close(pat->stdErr[1]);
+    close(pat->stdErr[0]);
+    close(pat->stdNum[1]);
+    close(pat->stdNum[0]);
 }
